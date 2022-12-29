05cd44a704d7054f902e95c82cd51def6b4a2cef
vasuklk97 <58772150+vasuklk97@users.noreply.github.com>
2022-12-29 14:53:06 +0530

    Update WeatherForecastController.cs

--- GithubExperimet/Controllers/WeatherForecastController.cs
+++ GithubExperimet/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace GithubExperimet.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]")] 
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
